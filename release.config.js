009992277090fccc4e50df972ec4795e12e74e25
amdefuse <amdefuse@gmail.com>
2024-07-25 16:09:21 +0100

    build: simplify release config

--- release.config.js
+++ release.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  extends: "./index.js",
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
 }
