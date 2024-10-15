ed019b39e7e344a90441dbc60e74e29efb4f7708
jobotics <jo_botics@proton.me>
2024-10-15 14:25:54 +0100

    chore(wip): add draft fn signAndSendTransactions

--- src/features/deposit/components/DepositWidget.tsx
+++ src/features/deposit/components/DepositWidget.tsx
@@ -1,9 +1,13 @@
 import { useActor } from "@xstate/react"
 import { useState } from "react"
 import { FormProvider, useForm } from "react-hook-form"
-import { depositNearMachine } from "src/features/machines/depositNearMachine"
-import { createActor, fromPromise } from "xstate"
+import {
+  Context,
+  depositNearMachine,
+} from "src/features/machines/depositNearMachine"
+import { type NonReducibleUnknown, createActor, fromPromise } from "xstate"
 import { DepositWidgetProvider } from "../../../providers"
+import { DepositService } from "../services/depositService"
 import { DepositFormController, DepositFormType } from "./DepositFormController"
 import {
   DepositFormNear,
@@ -11,10 +15,14 @@ import {
 } from "./Form/DepositFormNear"
 
 type DepositWidgetProps = {
+  accountId: string
   signAndSendTransactionsNear: (calldata: unknown) => void
 }
 
+const depositNearService = new DepositService()
+
 export const DepositWidget = ({
+  accountId,
   signAndSendTransactionsNear,
 }: DepositWidgetProps) => {
   const [formType, setFormType] = useState<DepositFormType | null>(null)
@@ -23,31 +31,44 @@ export const DepositWidget = ({
   const depositNearActor = createActor(
     depositNearMachine.provide({
       actors: {
-        signAndSendTransactions: fromPromise(async ({ input }) => {
-          console.log("signAndSendTransactions", input)
-          // TODO: create calldata payload
-          const txHash = (await signAndSendTransactionsNear(input)) as
-            | string
-            | undefined
-          return txHash || "" // TODO: Ensure a TX hash is returned
-        }),
+        signAndSendTransactions: fromPromise(
+          async ({ input }: { input: NonReducibleUnknown }) => {
+            const { asset, amount, accountId } = input as {
+              asset: string
+              amount: string
+              accountId: string
+            }
+            const calldata = depositNearService.createDepositNearTransaction(
+              accountId,
+              asset,
+              amount
+            )
+            // TODO: create calldata payload
+            const txHash = (await signAndSendTransactionsNear(input)) as
+              | string
+              | undefined
+            return txHash || "" // TODO: Ensure a TX hash is returned
+          }
+        ),
       },
-    }),
-    { input: {} }
+    })
   )
+
+  depositNearActor.subscribe((state) => {
+    console.log("Current state:", state.value)
+    console.log("Context:", state.context)
+  })
 
   const handleSubmitNear = async (values: DepositFormNearValues) => {
     console.log("handleSubmitNear", values)
+    depositNearActor.start()
     depositNearActor.send({
       type: "INPUT",
       asset: values.asset,
       amount: values.amount,
+      accountId,
     })
   }
-
-  depositNearActor.subscribe((snapshot) => {
-    console.log(snapshot.status)
-  })
 
   return (
     <DepositWidgetProvider>
--- src/features/deposit/services/depositService.ts
+++ src/features/deposit/services/depositService.ts
@@ -1,24 +1,47 @@
 import type { BlockchainEnum } from "../../../types/deposit"
 
 export interface DepositFacade {
-  generateDepositAddress: (blockchain: BlockchainEnum) => Promise<string>
-  directDepositViaNear: (
-    depositAddress: string,
-    depositAsset: string,
-    depositAmount: string
-  ) => Promise<unknown>
+  createDepositNearTransaction: (
+    senderAddress: string,
+    assetAddress: string,
+    amount: string
+  ) => unknown
+}
+export const FT_STORAGE_DEPOSIT_GAS = "30000000000000"
+export const FT_MINIMUM_STORAGE_BALANCE_LARGE = "12500000000000000000000"
+
+export enum TransactionMethod {
+  FT_ON_TRANSFER = "ft_on_transfer",
 }
 
 export class DepositService implements DepositFacade {
-  async generateDepositAddress(blockchain: BlockchainEnum) {
-    return "0x0"
-  }
-
-  async directDepositViaNear(
-    depositAddress: string,
-    depositAsset: string,
-    depositAmount: string
+  createDepositNearTransaction(
+    senderAddress: string,
+    assetAddress: string,
+    amount: string
   ) {
-    return true
+    console.log("new DepositService", senderAddress, assetAddress, amount)
+
+    const message = Buffer.from("").toString("base64")
+    return [
+      {
+        receiverId: "defuse.near",
+        actions: [
+          {
+            type: "FunctionCall",
+            params: {
+              methodName: TransactionMethod.FT_ON_TRANSFER,
+              args: {
+                sender_id: senderAddress,
+                amount,
+                message: "",
+              },
+              gas: FT_STORAGE_DEPOSIT_GAS,
+              deposit: FT_MINIMUM_STORAGE_BALANCE_LARGE,
+            },
+          },
+        ],
+      },
+    ]
   }
 }
