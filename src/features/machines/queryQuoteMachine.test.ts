5d1ae556cd8e0273f7fea887aafbdada644e154b
Old Emil <cawabunga@defuse.org>
2024-10-22 19:29:27 +0200

    feat: add real quoting via relay api

---
+++ src/features/machines/queryQuoteMachine.test.ts
@@ -0,0 +1,231 @@
+import { afterEach, describe, expect, it, vi } from "vitest"
+import {
+  aggregateQuotes,
+  calculateSplitAmounts,
+  queryQuote,
+} from "./queryQuoteMachine"
+
+import * as relayClient from "../../services/solverRelayHttpClient"
+
+vi.spyOn(relayClient, "quote")
+
+describe("queryQuote()", () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it("quotes full amount if total available is less than requested", async () => {
+    const input = {
+      tokensIn: ["token1"],
+      tokensOut: ["tokenOut"],
+      amountIn: 150n,
+      balances: { token1: 100n },
+    }
+
+    vi.mocked(relayClient.quote).mockImplementationOnce(async () => [
+      {
+        quote_hash: "q1",
+        defuse_asset_identifier_in: "token1",
+        defuse_asset_identifier_out: "tokenOut",
+        amount_in: "150",
+        amount_out: "200",
+        expiration_time: 500,
+      },
+    ])
+
+    const result = await queryQuote(input)
+
+    expect(relayClient.quote).toHaveBeenCalledTimes(1)
+    expect(relayClient.quote).toHaveBeenCalledWith({
+      defuse_asset_identifier_in: "token1",
+      defuse_asset_identifier_out: "tokenOut",
+      amount_in: "150",
+      min_deadline_ms: 120_000,
+    })
+    expect(result).toEqual({
+      amountsIn: { token1: 150n },
+      amountsOut: { tokenOut: 200n },
+      expirationTime: 500,
+      quoteHashes: ["q1"],
+      totalAmountIn: 150n,
+      totalAmountOut: 200n,
+    })
+  })
+
+  it("splits amount across tokens if available balance is sufficient", async () => {
+    const input = {
+      tokensIn: ["token1", "token2"],
+      tokensOut: ["tokenOut"],
+      amountIn: 150n,
+      balances: { token1: 100n, token2: 100n },
+    }
+
+    vi.mocked(relayClient.quote)
+      .mockImplementationOnce(async () => [
+        {
+          quote_hash: "q1",
+          defuse_asset_identifier_in: "token1",
+          defuse_asset_identifier_out: "tokenOut",
+          amount_in: "100",
+          amount_out: "20",
+          expiration_time: 500,
+        },
+      ])
+      .mockImplementationOnce(async () => [
+        {
+          quote_hash: "q2",
+          defuse_asset_identifier_in: "token2",
+          defuse_asset_identifier_out: "tokenOut",
+          amount_in: "50",
+          amount_out: "10",
+          expiration_time: 500,
+        },
+      ])
+
+    const result = await queryQuote(input)
+
+    expect(relayClient.quote).toHaveBeenCalledTimes(2)
+    expect(relayClient.quote).toHaveBeenCalledWith({
+      defuse_asset_identifier_in: "token1",
+      defuse_asset_identifier_out: "tokenOut",
+      amount_in: "100",
+      min_deadline_ms: 120_000,
+    })
+    expect(relayClient.quote).toHaveBeenCalledWith({
+      defuse_asset_identifier_in: "token2",
+      defuse_asset_identifier_out: "tokenOut",
+      amount_in: "50",
+      min_deadline_ms: 120_000,
+    })
+    expect(result).toEqual({
+      amountsIn: { token1: 100n, token2: 50n },
+      amountsOut: { tokenOut: 30n },
+      expirationTime: 500,
+      quoteHashes: ["q1", "q2"],
+      totalAmountIn: 150n,
+      totalAmountOut: 30n,
+    })
+  })
+
+  it("takes only first quote", async () => {
+    const input = {
+      tokensIn: ["token1"],
+      tokensOut: ["tokenOut"],
+      amountIn: 150n,
+      balances: { token1: 100n },
+    }
+
+    vi.mocked(relayClient.quote).mockImplementationOnce(async () => [
+      {
+        quote_hash: "q1",
+        defuse_asset_identifier_in: "token1",
+        defuse_asset_identifier_out: "tokenOut",
+        amount_in: "150",
+        amount_out: "200",
+        expiration_time: 500,
+      },
+      {
+        quote_hash: "q2",
+        defuse_asset_identifier_in: "token1",
+        defuse_asset_identifier_out: "tokenOut",
+        amount_in: "150",
+        amount_out: "180",
+        expiration_time: 500,
+      },
+    ])
+
+    const result = await queryQuote(input)
+
+    expect(result).toEqual({
+      amountsIn: { token1: 150n },
+      amountsOut: { tokenOut: 200n },
+      expirationTime: 500,
+      quoteHashes: ["q1"],
+      totalAmountIn: 150n,
+      totalAmountOut: 200n,
+    })
+  })
+
+  it("returns empty result if quote is null", async () => {
+    const input = {
+      tokensIn: ["token1"],
+      tokensOut: ["tokenOut"],
+      amountIn: 150n,
+      balances: { token1: 100n },
+    }
+
+    vi.mocked(relayClient.quote)
+      .mockImplementationOnce(async () => null)
+      .mockImplementationOnce(async () => [])
+
+    await expect(queryQuote(input)).resolves.toEqual({
+      amountsIn: {},
+      amountsOut: {},
+      expirationTime: 0,
+      quoteHashes: [],
+      totalAmountIn: 0n,
+      totalAmountOut: 0n,
+    })
+
+    await expect(queryQuote(input)).resolves.toEqual({
+      amountsIn: {},
+      amountsOut: {},
+      expirationTime: 0,
+      quoteHashes: [],
+      totalAmountIn: 0n,
+      totalAmountOut: 0n,
+    })
+  })
+})
+
+it("calculateSplitAmounts(): splits amounts correctly", () => {
+  const tokensIn = ["token1", "token2", "token3"]
+  const amountIn = 150n
+  const balances = {
+    token1: 100n,
+    token2: 50n,
+    token3: 200n,
+  }
+
+  const result = calculateSplitAmounts(tokensIn, amountIn, balances)
+  expect(result).toEqual({
+    token1: 100n,
+    token2: 50n,
+  })
+})
+
+it("aggregateQuotes(): aggregates quotes correctly", () => {
+  const quotes = [
+    [
+      {
+        quote_hash: "q1",
+        defuse_asset_identifier_in: "token1",
+        defuse_asset_identifier_out: "tokenOut",
+        amount_in: "100",
+        amount_out: "200",
+        expiration_time: 5000,
+      },
+    ],
+    [
+      {
+        quote_hash: "q2",
+        defuse_asset_identifier_in: "token2",
+        defuse_asset_identifier_out: "tokenOut",
+        amount_in: "50",
+        amount_out: "100",
+        expiration_time: 4000,
+      },
+    ],
+  ]
+
+  const result = aggregateQuotes(quotes)
+
+  expect(result).toEqual({
+    amountsIn: { token1: 100n, token2: 50n },
+    amountsOut: { tokenOut: 300n },
+    expirationTime: 4000,
+    quoteHashes: ["q1", "q2"],
+    totalAmountIn: 150n,
+    totalAmountOut: 300n,
+  })
+})
--- src/features/swap/components/SwapUIMachineProvider.tsx
+++ src/features/swap/components/SwapUIMachineProvider.tsx
@@ -1,5 +1,4 @@
 import { createActorContext } from "@xstate/react"
-import { parseUnits } from "ethers"
 import type { PropsWithChildren } from "react"
 import { useFormContext } from "react-hook-form"
 import { formatUnits } from "viem"
@@ -9,6 +8,7 @@ import type {
   WalletMessage,
   WalletSignatureResult,
 } from "../../../types"
+import { queryQuoteMachine } from "../../machines/queryQuoteMachine"
 import { swapIntentMachine } from "../../machines/swapIntentMachine"
 import { swapUIMachine } from "../../machines/swapUIMachine"
 import type { SwapFormValues } from "./SwapForm"
@@ -61,39 +61,7 @@ export function SwapUIMachineProvider({
             // We validate only `amountIn` and not entire form, because currently `amountOut` is also part of the form
             return trigger("amountIn")
           }),
-          queryQuote: fromPromise(async ({ input }) => {
-            const { amountIn } = getValues()
-
-            // todo: may throw if too many decimals, need to write safe parser
-            const amountInParsed = parseUnits(amountIn, assetIn.decimals)
-
-            // todo: replace code below with real quote request
-            console.warn("Do real quote request here")
-
-            // Simulate quote with a random factor around 1.5x amountInParsed
-            const baseAmountOut = (amountInParsed * 3n) / 2n
-
-            const randomFactor = (min: number, max: number) =>
-              BigInt(Math.floor(Math.random() * (max - min + 1) + min))
-
-            // Apply random factor within ±3%
-            const minFactor = 97
-            const maxFactor = 103
-            const randomMultiplier = randomFactor(minFactor, maxFactor)
-
-            const amountOutRandomized =
-              (baseAmountOut * randomMultiplier) / 100n
-
-            return {
-              quoteHashes: ["quoteHash"],
-              expirationTime: Math.floor(Date.now() / 1000) + 10 * 60,
-              totalAmountOut: amountOutRandomized,
-              amountsOut: {
-                // biome-ignore lint/style/noNonNullAssertion: <reason>
-                [input.tokensOut[0]!]: amountOutRandomized,
-              },
-            }
-          }),
+          queryQuote: queryQuoteMachine,
           // @ts-expect-error For some reason `swapIntentMachine` does not satisfy `swap` actor type
           swap: swapIntentMachine.provide({
             actors: {
