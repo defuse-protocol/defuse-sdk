adbdcc2a786e841eae50bd6673bc2261f66df663
Old Emil <cawabunga@defuse.org>
2024-10-27 08:48:34 +0100

    refactor: explicitly specify type of SwapUIMachineContext

--- src/features/swap/components/SwapUIMachineProvider.tsx
+++ src/features/swap/components/SwapUIMachineProvider.tsx
@@ -1,8 +1,13 @@
 import { createActorContext } from "@xstate/react"
-import type { PropsWithChildren } from "react"
+import type { PropsWithChildren, ReactElement, ReactNode } from "react"
 import { useFormContext } from "react-hook-form"
 import { formatUnits } from "viem"
-import { fromPromise } from "xstate"
+import {
+  type Actor,
+  type ActorOptions,
+  type SnapshotFrom,
+  fromPromise,
+} from "xstate"
 import type {
   SwappableToken,
   WalletMessage,
@@ -12,7 +17,35 @@ import { swapIntentMachine } from "../../machines/swapIntentMachine"
 import { swapUIMachine } from "../../machines/swapUIMachine"
 import type { SwapFormValues } from "./SwapForm"
 
-export const SwapUIMachineContext = createActorContext(swapUIMachine)
+/**
+ * We explicitly define the type of `swapUIMachine` to avoid:
+ * ```
+ * TS7056: The inferred type of this node exceeds the maximum length the
+ * compiler will serialize. An explicit type annotation is needed.
+ * ```
+ *
+ * Either the machine type is too complex or we incorrectly specify types inside it.
+ * Either way it is just a workaround for TypeScript limitations.
+ * Copy-paste the type from `@xstate/react/dist/declarations/src/createActorContext.d.ts`
+ */
+interface SwapUIMachineContextInterface {
+  useSelector: <T>(
+    selector: (snapshot: SnapshotFrom<typeof swapUIMachine>) => T,
+    compare?: (a: T, b: T) => boolean
+  ) => T
+  useActorRef: () => Actor<typeof swapUIMachine>
+  Provider: (props: {
+    children: ReactNode
+    options?: ActorOptions<typeof swapUIMachine>
+    /** @deprecated Use `logic` instead. */
+    machine?: never
+    logic?: typeof swapUIMachine
+    // biome-ignore lint/suspicious/noExplicitAny: it is fine `any` here
+  }) => ReactElement<any, any>
+}
+
+export const SwapUIMachineContext: SwapUIMachineContextInterface =
+  createActorContext(swapUIMachine)
 
 interface SwapUIMachineProviderProps extends PropsWithChildren {
   initialTokenIn: SwappableToken
