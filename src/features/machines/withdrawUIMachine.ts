3ccc16192e709d1cb61080551c5fa2d0ccfaedcf
Old Emil <cawabunga@defuse.org>
2024-10-31 09:49:04 +0100

    refactor: clone swapUIMachine -> withdrawUIMachine

---
+++ src/features/machines/withdrawUIMachine.ts
@@ -0,0 +1,454 @@
+import { parseUnits } from "ethers"
+import type { providers } from "near-api-js"
+import {
+  type ActorRefFrom,
+  assertEvent,
+  assign,
+  emit,
+  fromPromise,
+  sendTo,
+  setup,
+  spawnChild,
+} from "xstate"
+import { settings } from "../../config/settings"
+import type { SwappableToken } from "../../types"
+import type { BaseTokenInfo, UnifiedTokenInfo } from "../../types/base"
+import type { Transaction } from "../../types/deposit"
+import {
+  type ChildEvent as BackgroundQuoterEvents,
+  backgroundQuoterMachine,
+} from "./backgroundQuoterMachine"
+import {
+  type BalanceMapping,
+  type Events as DepositedBalanceEvents,
+  depositedBalanceMachine,
+} from "./depositedBalanceMachine"
+import { intentStatusMachine } from "./intentStatusMachine"
+import type { AggregatedQuote } from "./queryQuoteMachine"
+import {
+  type Output as SwapIntentMachineOutput,
+  swapIntentMachine,
+} from "./swapIntentMachine"
+
+export type Context = {
+  error: Error | null
+  quote: AggregatedQuote | null
+  formValues: {
+    tokenIn: SwappableToken
+    tokenOut: SwappableToken
+    amountIn: string
+  }
+  parsedFormValues: {
+    amountIn: bigint
+  }
+  intentCreationResult: SwapIntentMachineOutput | null
+  intentRefs: ActorRefFrom<typeof intentStatusMachine>[]
+  tokenList: SwappableToken[]
+}
+
+type PassthroughEvent = {
+  type: "INTENT_SETTLED"
+  data: {
+    intentHash: string
+    txHash: string
+    tokenIn: BaseTokenInfo | UnifiedTokenInfo
+    tokenOut: BaseTokenInfo | UnifiedTokenInfo
+    quote: AggregatedQuote
+  }
+}
+
+export const withdrawUIMachine = setup({
+  types: {
+    input: {} as {
+      tokenIn: SwappableToken
+      tokenOut: SwappableToken
+      tokenList: SwappableToken[]
+    },
+    context: {} as Context,
+    events: {} as
+      | {
+          type: "input"
+          params: Partial<{
+            tokenIn: SwappableToken
+            tokenOut: SwappableToken
+            amountIn: string
+          }>
+        }
+      | {
+          type: "submit"
+          params: {
+            userAddress: string
+            nearClient: providers.Provider
+            sendNearTransaction: (
+              tx: Transaction
+            ) => Promise<{ txHash: string } | null>
+          }
+        }
+      | {
+          type: "BALANCE_CHANGED"
+          params: {
+            changedBalanceMapping: BalanceMapping
+          }
+        }
+      | BackgroundQuoterEvents
+      | DepositedBalanceEvents
+      | PassthroughEvent,
+
+    emitted: {} as PassthroughEvent,
+
+    children: {} as {
+      depositedBalanceRef: "depositedBalanceActor"
+      swapRef: "swapActor"
+    },
+  },
+  actors: {
+    backgroundQuoterActor: backgroundQuoterMachine,
+    depositedBalanceActor: depositedBalanceMachine,
+    formValidationActor: fromPromise(async (): Promise<boolean> => {
+      throw new Error("not implemented")
+    }),
+    swapActor: swapIntentMachine,
+    intentStatusActor: intentStatusMachine,
+  },
+  actions: {
+    setFormValues: assign({
+      formValues: (
+        { context },
+        {
+          data,
+        }: {
+          data: Partial<{
+            tokenIn: SwappableToken
+            tokenOut: SwappableToken
+            amountIn: string
+          }>
+        }
+      ) => ({
+        ...context.formValues,
+        ...data,
+      }),
+    }),
+    parseFormValues: assign({
+      parsedFormValues: ({ context }) => {
+        try {
+          return {
+            amountIn: parseUnits(
+              context.formValues.amountIn,
+              context.formValues.tokenIn.decimals
+            ),
+          }
+        } catch {
+          return {
+            amountIn: 0n,
+          }
+        }
+      },
+    }),
+    updateUIAmountOut: () => {
+      throw new Error("not implemented")
+    },
+    setQuote: assign({
+      quote: (_, value: AggregatedQuote) => value,
+    }),
+    clearQuote: assign({ quote: null }),
+    clearError: assign({ error: null }),
+    setIntentCreationResult: assign({
+      intentCreationResult: (_, value: SwapIntentMachineOutput) => value,
+    }),
+    clearIntentCreationResult: assign({ intentCreationResult: null }),
+    passthroughEvent: emit((_, event: PassthroughEvent) => event),
+
+    spawnBackgroundQuoterRef: spawnChild("backgroundQuoterActor", {
+      id: "backgroundQuoterRef",
+      input: ({ self }) => ({
+        parentRef: self,
+        delayMs: settings.quotePollingIntervalMs,
+      }),
+    }),
+    // Warning: This cannot be properly typed, so you can send an incorrect event
+    sendToBackgroundQuoterRefNewQuoteInput: sendTo(
+      "backgroundQuoterRef",
+      ({ context, self }) => {
+        const snapshot = self.getSnapshot()
+
+        // However knows how to access the child's state, please update this
+        const depositedBalanceRef:
+          | ActorRefFrom<typeof depositedBalanceMachine>
+          | undefined = snapshot.children.depositedBalanceRef
+        const balances = depositedBalanceRef?.getSnapshot().context.balances
+
+        return {
+          type: "NEW_QUOTE_INPUT",
+          params: {
+            tokenIn: context.formValues.tokenIn,
+            tokenOut: context.formValues.tokenOut,
+            amountIn: context.parsedFormValues.amountIn,
+            balances: balances ?? {},
+          },
+        }
+      }
+    ),
+    // Warning: This cannot be properly typed, so you can send an incorrect event
+    sendToBackgroundQuoterRefPause: sendTo("backgroundQuoterRef", {
+      type: "PAUSE",
+    }),
+
+    spawnDepositedBalanceRef: spawnChild("depositedBalanceActor", {
+      id: "depositedBalanceRef",
+      input: ({ self, context }) => ({
+        parentRef: self,
+        tokenList: context.tokenList,
+      }),
+    }),
+    relayToDepositedBalanceRef: sendTo(
+      "depositedBalanceRef",
+      (_, event: DepositedBalanceEvents) => event
+    ),
+    sendToDepositedBalanceRefRefresh: sendTo("depositedBalanceRef", (_) => ({
+      type: "REQUEST_BALANCE_REFRESH",
+    })),
+
+    // Warning: This cannot be properly typed, so you can send an incorrect event
+    sendToSwapRefNewQuote: sendTo(
+      "swapRef",
+      (_, event: BackgroundQuoterEvents) => event
+    ),
+
+    spawnIntentStatusActor: assign({
+      intentRefs: (
+        { context, spawn, self },
+        output: SwapIntentMachineOutput
+      ) => {
+        if (output.status !== "INTENT_PUBLISHED") return context.intentRefs
+
+        // todo: take quote from result of `swap`
+        assert(context.quote != null, "quote is null")
+
+        const intentRef = spawn("intentStatusActor", {
+          id: `intent-${output.intentHash}`,
+          input: {
+            parentRef: self,
+            intentHash: output.intentHash,
+            tokenIn: context.formValues.tokenIn,
+            tokenOut: context.formValues.tokenOut,
+            quote: context.quote,
+          },
+        })
+
+        return [intentRef, ...context.intentRefs]
+      },
+    }),
+  },
+  guards: {
+    isQuoteRelevant: ({ context }) => {
+      // todo: implement real check for fetched quotes if they're expired or not
+      console.warn(
+        "Implement real check for fetched quotes if they're expired or not"
+      )
+      return context.quote != null && context.quote.totalAmountOut > 0n
+    },
+  },
+}).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5SwO4EMAOBaArgSwGIBJAOQBUBRcgfQGUKyyAZCgEQG0AGAXUVAwD2sPABc8AgHZ8QAD0RYArAoAcAOgCMAdgWcALJ04BOZZoBMCgDQgAnvMO7Nq5ac6b1B07oWfDAX19WqJi4hABCAIJM4SQAwhTUMQAS0QDibFy8SCCCwmKS0nIIAMxFCk5GygBsmpqGnJUKlZVWtghYhpWmqtWG6lWuupWcRer+gejY+ARMAPIppBnSOaLiUlmFlUVOvbqmlbpFulpGmi12uoaqRW7Kt-aaRfUKYyBBk4SzKTMAqmSLWcs8mtQIUsKZ1JUNPpDOZwcoFJomsozm17Jdrn07g5Hg0Xm8QqpICsJFACLAcAAjAC2on+-CEK3y60Q6i0qlq7lMhz6Sm0KKwRUMl06HSFpmURiafgCrwmBKJYhJBDwEgwOBEdOyDKBBXkezK4PUOgUEKM4uu-Oul3UB046iKm3tQsqeLl+EJEGJpJIFAA6tQAIrfGaUTWA1a6trY1RC0qCkx1fRFfn7MrqcxNLxwkyjGX490KlVQVQANzQABs8BA0IrSRBJGBVCqSwIANaN-N4D1e0sVqs1osIZsCADGA8kGTD2ojzLaKi2pkMCl0yncVWUu2aNnk2iuCKM6nsnCUxiKruCBc9td7lertYI9YkjeH7dUne717Lt4HJKHEhbY5ApO6iZPSuQziC8gqGUwzHnaK4SoM-I2l0drKEUhq6IMaEunmbpduS1KiPej7Pv+bYdhMABKYAAGZTuBTKQQgRzqKoi6rlhWHrqypzbm0jxdIMG7rocDpHJo57vG+lI0iI95gAATopAiKaoGDljWtGqVSb7UXRDGMsCsiIAclyGgoIwNFU9ilMh+zsrs9gmrseyaLsUkEoRcn3j6-pBiGFCGTqs4HDBvSsuKejHJY-GKA4Vwig66GVPYnSeZePboF61AAI44AIIhgAQfmBsGoY8Es05MSZqKuKoCgwhKWhciYRTInFpRsaxvRDNcIzeP4MoSAIEBwNInZVYxxmgu4wwNU1dpmO1Dwda0WASga7mcIhpieJsZ54ReXaFiSU1GZG7RKAtUUtSt7X8iaC5Lg6pRmNiklHdJp3FlW5ZgOdIXMVgXiOAc4reCMMK1Hx62Lpw7FOs4y72lhhiHeMx0fkWN79rWgMQbVYKClcZguAi65Crc-I1E4tTo6YmjOO19oY7KWPecRRYEzVhTtWURxDOKhrVAi-JHDBe0mntJR2ocuGY99V449ltZ5QVRU8zNO5DE4m37vCuwQo9uiqIM3hPJsRu9UNvhAA */
+  id: "swap-ui",
+
+  context: ({ input }) => ({
+    error: null,
+    quote: null,
+    formValues: {
+      tokenIn: input.tokenIn,
+      tokenOut: input.tokenOut,
+      amountIn: "",
+    },
+    parsedFormValues: {
+      amountIn: 0n,
+    },
+    intentCreationResult: null,
+    intentRefs: [],
+    tokenList: input.tokenList,
+  }),
+
+  entry: ["spawnBackgroundQuoterRef", "spawnDepositedBalanceRef"],
+
+  on: {
+    INTENT_SETTLED: {
+      actions: [
+        {
+          type: "passthroughEvent",
+          params: ({ event }) => event,
+        },
+        "sendToDepositedBalanceRefRefresh",
+      ],
+    },
+
+    BALANCE_CHANGED: {
+      actions: [
+        ({ event }) => {
+          console.log("Balance changed", event.params.changedBalanceMapping)
+        },
+        "sendToBackgroundQuoterRefNewQuoteInput",
+      ],
+    },
+
+    LOGIN: {
+      actions: {
+        type: "relayToDepositedBalanceRef",
+        params: ({ event }) => event,
+      },
+    },
+    LOGOUT: {
+      actions: {
+        type: "relayToDepositedBalanceRef",
+        params: ({ event }) => event,
+      },
+    },
+  },
+
+  states: {
+    editing: {
+      on: {
+        submit: {
+          target: "submitting",
+          guard: "isQuoteRelevant",
+          actions: "clearIntentCreationResult",
+        },
+
+        input: {
+          target: ".validating",
+          actions: [
+            "clearQuote",
+            "updateUIAmountOut",
+            "sendToBackgroundQuoterRefPause",
+            "clearError",
+            {
+              type: "setFormValues",
+              params: ({ event }) => ({ data: event.params }),
+            },
+            "parseFormValues",
+          ],
+        },
+
+        NEW_QUOTE: {
+          actions: [
+            {
+              type: "setQuote",
+              params: ({ event }) => event.params.quote,
+            },
+            "updateUIAmountOut",
+          ],
+        },
+      },
+
+      states: {
+        idle: {},
+
+        validating: {
+          invoke: {
+            src: "formValidationActor",
+
+            onDone: [
+              {
+                target: "waiting_quote",
+                guard: ({ event }) => event.output,
+                actions: "sendToBackgroundQuoterRefNewQuoteInput",
+                reenter: true,
+              },
+              "idle",
+            ],
+          },
+        },
+
+        waiting_quote: {
+          on: {
+            NEW_QUOTE: {
+              target: "idle",
+              actions: [
+                {
+                  type: "setQuote",
+                  params: ({ event }) => event.params.quote,
+                },
+                "updateUIAmountOut",
+              ],
+              description: `should do the same as NEW_QUOTE on "editing" iteself`,
+            },
+          },
+        },
+      },
+
+      initial: "idle",
+      entry: "updateUIAmountOut",
+    },
+
+    submitting: {
+      invoke: {
+        id: "swapRef",
+        src: "swapActor",
+
+        input: ({ context, event }) => {
+          assertEvent(event, "submit")
+
+          const quote = context.quote
+          if (!quote) {
+            throw new Error("quote not available")
+          }
+
+          return {
+            userAddress: event.params.userAddress,
+            nearClient: event.params.nearClient,
+            sendNearTransaction: event.params.sendNearTransaction,
+            quote,
+            tokenIn: context.formValues.tokenIn,
+            tokenOut: context.formValues.tokenOut,
+            amountIn: context.parsedFormValues.amountIn,
+          }
+        },
+
+        onDone: {
+          target: "editing",
+
+          actions: [
+            {
+              type: "spawnIntentStatusActor",
+              params: ({ event }) => event.output,
+            },
+            {
+              type: "setIntentCreationResult",
+              params: ({ event }) => event.output,
+            },
+          ],
+        },
+
+        onError: {
+          target: "editing",
+
+          actions: ({ event }) => {
+            console.error(event.error)
+          },
+        },
+      },
+
+      on: {
+        NEW_QUOTE: {
+          actions: [
+            {
+              type: "setQuote",
+              params: ({ event }) => event.params.quote,
+            },
+            {
+              type: "sendToSwapRefNewQuote",
+              params: ({ event }) => event,
+            },
+          ],
+        },
+      },
+    },
+  },
+
+  initial: "editing",
+})
+
+function assert(condition: unknown, msg?: string): asserts condition {
+  if (!condition) {
+    throw new Error(msg)
+  }
+}
