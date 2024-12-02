f3206dda9d28c4ce01db1253ed82cee678014a1f
Old Emil <cawabunga@defuse.org>
2024-12-02 17:27:30 +0100

    fix: do include style in SwapWidgetProvider

--- src/providers/SwapWidgetProvider.tsx
+++ src/providers/SwapWidgetProvider.tsx
@@ -1,12 +1,9 @@
 import type React from "react"
 import type { PropsWithChildren } from "react"
-
 import { ModalContainer } from "../components/Modal/ModalContainer"
-
 import { ModalStoreProvider } from "./ModalStoreProvider"
-import { TokensStoreProvider } from "./TokensStoreProvider"
-import "../styles/main.css"
 import { QueryClientProvider } from "./QueryClientProvider"
+import { TokensStoreProvider } from "./TokensStoreProvider"
 
 export const SwapWidgetProvider: React.FC<PropsWithChildren> = ({
   children,
