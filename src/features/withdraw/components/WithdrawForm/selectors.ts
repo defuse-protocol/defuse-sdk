a3176036bc2b88e8d576118aeb44521b8c6762bd
Old Emil <cawabunga@defuse.org>
2024-12-10 19:20:15 +0100

    fix: fix showing no liquidity on withdrawal

--- src/features/withdraw/components/WithdrawForm/selectors.ts
+++ src/features/withdraw/components/WithdrawForm/selectors.ts
@@ -1,36 +1,13 @@
 import type { SnapshotFrom } from "xstate"
-import { isAggregatedQuoteEmpty } from "../../../../services/quoteService"
 import type { withdrawUIMachine } from "../../../machines/withdrawUIMachine"
 
-/**
- * @return null | boolean - null if not enough info to determine
- */
 export function isLiquidityUnavailableSelector(
   state: SnapshotFrom<typeof withdrawUIMachine>
-): boolean | null {
-  if (
-    state.context.preparationOutput == null ||
-    state.context.preparationOutput.tag === "err"
-  ) {
-    // No withdraw info
-    return null
-  }
-
-  const swap = state.context.preparationOutput.value.swap
-  if (swap != null && isAggregatedQuoteEmpty(swap.swapQuote)) {
-    return true
-  }
-
-  const nep141Storage = state.context.preparationOutput.value.nep141Storage
-  if (
-    nep141Storage != null &&
-    nep141Storage.type === "swap_needed" &&
-    isAggregatedQuoteEmpty(nep141Storage.quote)
-  ) {
-    return true
-  }
-
-  return false
+): boolean {
+  return (
+    state.context.preparationOutput?.tag === "err" &&
+    state.context.preparationOutput.value.reason === "ERR_EMPTY_QUOTE"
+  )
 }
 
 /**
