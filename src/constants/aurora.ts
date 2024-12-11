746f64bfb9b5f87b82394fd87b8c25b6e9dc6840
Old Emil <cawabunga@defuse.org>
2024-12-11 14:05:13 +0100

    chore: add account id and siloToSilo contract address for aurora

--- src/constants/aurora.ts
+++ src/constants/aurora.ts
@@ -2,6 +2,7 @@
  * SiloToSilo addresses on blockchains within Aurora Engine
  */
 export const siloToSiloAddress = {
+  aurora: "0x055707c67977e8217F98f19cFa8aca18B2282D0C",
   turbochain: "0x8a4Bf14C51e1092581F1392810eE38c5A20f83da",
 } as const
 
@@ -10,5 +11,6 @@ export const siloToSiloAddress = {
  * Mapping: ChainName -> AccountId
  */
 export const auroraEngineContractId = {
+  aurora: "aurora",
   turbochain: "0x4e45415f.c.aurora",
 } as const
