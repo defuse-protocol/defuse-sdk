c8903f63c9768bfba7c6ba5862431d5f85e847a4
depressedPlumber502 <depressed.plumber.502@defuse.org>
2024-12-12 14:06:18 +0400

    fix: balance block layout shift (#164)
    * fix: balance block layout shift
    
    * feat: add skeleton to BlockMultiBalance
    
    * feat: remove skeleton
    
    * chore: formatting

--- src/components/Form/FieldComboInput.tsx
+++ src/components/Form/FieldComboInput.tsx
@@ -108,9 +108,7 @@ export const FieldComboInput = <T extends FieldValues>({
   return (
     <div
       className={clsx(
-        "relative flex flex-col px-5 py-[2.375rem] w-full bg-gray-50 dark:bg-black-900 dark:border-black-950",
-        !label && "pt-5",
-        !price && balance == null && errors && !errors[fieldName] && "pb-5",
+        "relative flex flex-col px-5 pt-5 pb-10 w-full bg-gray-50 dark:bg-black-900 dark:border-black-950",
         className
       )}
     >
