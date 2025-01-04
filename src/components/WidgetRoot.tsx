9e6bff367eb83f41e0855b0bf72639bf1ff5d6cb
Old Emil <cawabunga@defuse.org>
2025-01-04 20:45:19 +0100

    feat: add isolated container for portals

--- src/components/WidgetRoot.tsx
+++ src/components/WidgetRoot.tsx
@@ -1,18 +1,40 @@
-import type { ReactNode } from "react"
+import { type ReactNode, createContext, useState } from "react"
+import { createPortal } from "react-dom"
 import root from "react-shadow"
 import style from "../styles/main.css"
+
+export const WidgetContext = createContext<{
+  portalContainer: null | HTMLElement
+}>({
+  portalContainer: null,
+})
 
 export interface WidgetRootProps {
   children: ReactNode
 }
 
 export function WidgetRoot(props: WidgetRootProps) {
-  return (
-    // @ts-expect-error react-shadow is not correctly typed, `root.div` is correct element
-    <root.div>
-      <style type="text/css">{style}</style>
+  const [portalContainer, setPortalContainer] = useState<HTMLElement | null>(
+    null
+  )
 
-      {props.children}
-    </root.div>
+  return (
+    <WidgetContext.Provider value={{ portalContainer }}>
+      {/*@ts-expect-error react-shadow is not correctly typed, `root.div` is correct element*/}
+      <root.div>
+        <style type="text/css">{style}</style>
+
+        {props.children}
+      </root.div>
+
+      {createPortal(
+        // @ts-expect-error react-shadow is not correctly typed, `root.div` is correct element
+        <root.div>
+          <style type="text/css">{style}</style>
+          <div ref={setPortalContainer} />
+        </root.div>,
+        globalThis?.document?.body
+      )}
+    </WidgetContext.Provider>
   )
 }
