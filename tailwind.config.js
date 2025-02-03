321e12641e8ac4dfe04b7ebbd89897821f66fa01
Old Emil <cawabunga@defuse.org>
2025-02-03 11:58:10 +0100

    chore: add accordion toggle animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -153,10 +153,20 @@ const config = {
             transform: "translateY(0)",
           },
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         "content-show": "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
         "slide-up": "slideUpAndFade 300ms cubic-bezier(0.16, 1, 0.3, 1)",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
