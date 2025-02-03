881d4be935f0335755628b0daad625b0dcf63d13
Old Emil <cawabunga@defuse.org>
2025-02-03 09:40:26 +0100

    refactor: replace div with button; remove BRs

--- src/features/deposit/components/DepositForm/ActiveDeposit.tsx
+++ src/features/deposit/components/DepositForm/ActiveDeposit.tsx
@@ -174,18 +174,20 @@ function Balance({
       {isFungibleToken(token) && token.address === "wrap.near" && (
         <TooltipInfo
           icon={
-            <Text asChild color={accentColor}>
-              <InfoCircledIcon />
-            </Text>
+            <button type="button">
+              <Text asChild color={accentColor}>
+                <InfoCircledIcon />
+              </Text>
+            </button>
           }
         >
-          Combined balance of NEAR and wNEAR.
-          <br /> NEAR will be automatically wrapped to wNEAR
-          <br /> if your wNEAR balance isn't sufficient for the swap.
+          Combined balance of NEAR and wNEAR. NEAR will be automatically wrapped
+          to wNEAR if your wNEAR balance isn't sufficient for the swap.
+          <br />
           <br />
           Note that to cover network fees, we reserve
-          {` ${formatTokenValue(RESERVED_NEAR_BALANCE, token.decimals)} NEAR`}
-          <br /> in your wallet.
+          {` ${formatTokenValue(RESERVED_NEAR_BALANCE, token.decimals)} NEAR `}
+          in your wallet.
         </TooltipInfo>
       )}
     </div>
