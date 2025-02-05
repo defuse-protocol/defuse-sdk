8774fe39a09ecc2a151a1135e6d91bd40ea3cd94
jobotics <jo_botics@proton.me>
2025-02-05 17:17:28 +0000

    feat: migrate broadcasting to intent broadcast machine

---
+++ src/features/machines/intentBroadcastMachine.ts
@@ -0,0 +1,383 @@
+import type { providers } from "near-api-js"
+import { settings } from "src/config/settings"
+import { assign, fromPromise, setup } from "xstate"
+import { logger } from "../../logger"
+import { publishIntent } from "../../services/intentService"
+import type { AggregatedQuote } from "../../services/quoteService"
+import type { Nep413DefuseMessageFor_DefuseIntents } from "../../types/defuse-contracts-types"
+import type { ChainType } from "../../types/deposit"
+import type { WalletMessage, WalletSignatureResult } from "../../types/swap"
+import { assert } from "../../utils/assert"
+import type { DefuseUserId } from "../../utils/defuse"
+import {
+  makeInnerSwapMessage,
+  makeSwapMessage,
+} from "../../utils/messageFactory"
+import type { PriorityQueue } from "../../utils/priorityQueue"
+import {
+  accountSlippageExactIn,
+  computeTotalDeltaDifferentDecimals,
+  negateTokenValue,
+} from "../../utils/tokenUtils"
+import type { ParentEvents as BackgroundQuoterEvents } from "./backgroundQuoterMachine"
+import {
+  type Errors,
+  type IntentDescription,
+  type IntentOperationParams,
+  calcOperationAmountOut,
+  dequeueValidQuote,
+  enqueueBetterQuote,
+} from "./intentSignMachine"
+import {
+  type SendNearTransaction,
+  publicKeyVerifierMachine,
+} from "./publicKeyVerifierMachine"
+
+type Context = {
+  userAddress: string
+  userChainType: ChainType
+  defuseUserId: DefuseUserId
+  referral?: string
+  slippageBasisPoints: number
+  nearClient: providers.Provider
+  sendNearTransaction: SendNearTransaction
+  intentOperationParams: IntentOperationParams
+  quoteToPublish: AggregatedQuote | null
+  quotes: PriorityQueue<AggregatedQuote>
+  messageToSign: {
+    walletMessage: WalletMessage
+    innerMessage: Nep413DefuseMessageFor_DefuseIntents
+  }
+  signature: WalletSignatureResult
+  intentDescription: IntentDescription
+  intentHash: string | null
+  error: Errors
+}
+
+type Input = {
+  userAddress: string
+  userChainType: ChainType
+  defuseUserId: DefuseUserId
+  referral?: string
+  slippageBasisPoints: number
+  nearClient: providers.Provider
+  sendNearTransaction: SendNearTransaction
+  intentOperationParams: IntentOperationParams
+  quoteToPublish: AggregatedQuote | null
+  quotes: PriorityQueue<AggregatedQuote>
+  messageToSign: {
+    walletMessage: WalletMessage
+    innerMessage: Nep413DefuseMessageFor_DefuseIntents
+  }
+  signature: WalletSignatureResult
+  intentDescription: IntentDescription
+}
+
+export type Output =
+  | NonNullable<Context["error"]>
+  | {
+      tag: "ok"
+      value: {
+        intentHash: string
+        intentDescription: IntentDescription
+      }
+    }
+
+type Events = BackgroundQuoterEvents
+
+export const intentBroadcastMachine = setup({
+  types: {
+    context: {} as Context,
+    input: {} as Input,
+    output: {} as Output,
+    events: {} as Events,
+  },
+  actions: {
+    setError: assign({
+      error: (_, error: NonNullable<Context["error"]>["value"]) => ({
+        tag: "err" as const,
+        value: error,
+      }),
+    }),
+    logError: (_, params: { error: unknown }) => {
+      logger.error(params.error)
+    },
+    proposeQuote: ({ context }, proposedQuote: AggregatedQuote) => {
+      if (context.intentOperationParams.quote) {
+        enqueueBetterQuote(
+          context.quotes,
+          context.intentOperationParams.quote,
+          proposedQuote,
+          context.intentOperationParams.tokenOut,
+          context.slippageBasisPoints
+        )
+      }
+    },
+    assembleSignMessages: assign({
+      messageToSign: ({ context }) => {
+        assert(
+          context.intentOperationParams.type === "swap",
+          "Operation must be swap"
+        )
+
+        const innerMessage = makeInnerSwapMessage({
+          tokenDeltas: accountSlippageExactIn(
+            context.intentOperationParams.quote.tokenDeltas,
+            context.slippageBasisPoints
+          ),
+          signerId: context.defuseUserId,
+          deadlineTimestamp: Date.now() + settings.swapExpirySec * 1000,
+          referral: context.referral,
+        })
+
+        return {
+          innerMessage,
+          walletMessage: makeSwapMessage({
+            innerMessage,
+            recipient: settings.defuseContractId,
+          }),
+        }
+      },
+    }),
+    dequeueValidQuote: assign({
+      quoteToPublish: ({ context }) => dequeueValidQuote(context.quotes),
+    }),
+    setIntentHash: assign({
+      intentHash: (_, intentHash: string) => intentHash,
+    }),
+  },
+  actors: {
+    publicKeyVerifierActor: publicKeyVerifierMachine,
+    broadcastMessage: fromPromise(
+      async ({
+        input,
+      }: {
+        input: {
+          signatureData: WalletSignatureResult
+          userInfo: { userAddress: string; userChainType: ChainType }
+          quoteHashes: string[]
+        }
+      }) =>
+        publishIntent(input.signatureData, input.userInfo, input.quoteHashes)
+    ),
+  },
+  guards: {
+    isIntentRelevant: ({ context }) => {
+      const hadQuote = context.intentOperationParams.quote != null
+      const hasQuote = context.quoteToPublish != null
+      return hadQuote === hasQuote
+    },
+    isSigned: (_, params: WalletSignatureResult | null) => params != null,
+    isTrue: (_, params: boolean) => params,
+    isOk: (_, params: { tag: "ok" } | { tag: "err" }) => params.tag === "ok",
+    isQuoteOk: ({ event }) => {
+      return event.params.quote.tag === "ok"
+    },
+  },
+}).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QEsB2AXMGC0AjATgPYCGEAxsbOgMQByAogOoD6AigKoDyAKvQNoAGALqJQAB0Kxk6ZIVSiQAD0QAmAGwqAdAFYAjAA4A7AGYNK47pUBObQBoQAT0TZ1AFh2vjn3catXDeq4AvkH2aJg4BCTklOiayBAANmDUgiJIIBJSMnIKyggqrto6VgKuKtrGhoYqKvqu9k4FmmXaAhZWxgJq-mptxiFhGFjoeESkFFSaAGpg+MgAZg5oUAAEAJLDGKnCClnSsvIZ+bpW7irVVroCugaenY2quu7G+moW6gJWfdqGaoMgcIjMbRSZxWbzJYrDZbGh8XTpcSSA65Y6IVwmTTlWpVfTafTdKqPBC6AKaPr+NrXb7afEAoGRcYxKYAISZYOhmwiNAgcjA8VQADdCABrfkM0ZRCaxTRs0GxTmwhBoYUUHKoNJpPbI9V5RA+KyaV7GXFveq+YzEooCI3XNQ+fHWX702Eg6Ws9kK1BrLkjai81DioWioPct3MuJy90yb0w7nK4Nqw6ahHa7KHPUIFzGLRdVyUyzXIwNRzotq27p1eqk1xqATaF1hqUR2WeqiK7nUOZEfCaMSJYjoBaEfAAWwFTbbkanHZGCdVg+Twi1GX2urRWdJhha+P0+h8hiLhhLTTqxk0KgENRUpJz7V0DYBqEIEDgCgl4bBaZRR1A+RcBJGmUBY3gIxbEtgD66Jo1zHhY+7XL8NSNsCzZgvESRgN+65-qorj6Joe4mmcljXvoXgQe85JXq4BjGLSt5gShjLylMEKLMssa+hg2EZhuuhqNutSHq4tEXLU5EnogZwtKJBjaO8XTCfozGSlOrasTGPqwrxqK4SSfSaFY9Q2GoGhkVJCAdDozxeK4NyFFeqmfjKADChCjv2YCYBAum-ko+p+La2i0XZAiXrWKjEho0EhQe9m2WZ-yhICrpoTKADiWBzMgZCrPQ+A9n5maQb8RqHqY9qWNY5F2KWCAxR48U3J4SUhCEQA */
+  context: ({ input }) => {
+    return {
+      ...input,
+      intentHash: null,
+      error: null,
+    }
+  },
+
+  id: "intent-broadcast",
+
+  initial: "idle",
+
+  output: ({ context }): Output => {
+    if (context.intentHash != null) {
+      const intentType = context.intentOperationParams.type
+      switch (intentType) {
+        case "swap": {
+          const quote = context.quoteToPublish
+          assert(quote != null, "Quote must be set for swap intent")
+
+          return {
+            tag: "ok",
+            value: {
+              intentHash: context.intentHash,
+              intentDescription: {
+                type: "swap",
+                totalAmountIn: negateTokenValue(
+                  computeTotalDeltaDifferentDecimals(
+                    context.intentOperationParams.tokensIn,
+                    quote.tokenDeltas
+                  )
+                ),
+                totalAmountOut: computeTotalDeltaDifferentDecimals(
+                  [context.intentOperationParams.tokenOut],
+                  quote.tokenDeltas
+                ),
+              },
+            },
+          }
+        }
+        case "withdraw": {
+          return {
+            tag: "ok",
+            value: {
+              intentHash: context.intentHash,
+              intentDescription: {
+                type: "withdraw",
+                amountWithdrawn: calcOperationAmountOut(
+                  context.intentOperationParams,
+                  context.quoteToPublish
+                ),
+              },
+            },
+          }
+        }
+        default:
+          intentType satisfies never
+          throw new Error("exhaustive check failed")
+      }
+    }
+
+    if (context.error != null) {
+      return context.error
+    }
+
+    throw new Error("Unexpected output")
+  },
+
+  on: {
+    NEW_QUOTE: {
+      guard: "isQuoteOk",
+      actions: [
+        {
+          type: "proposeQuote",
+          params: ({ event }) => event.params.quote.value as AggregatedQuote,
+        },
+      ],
+    },
+  },
+  states: {
+    idle: {
+      always: "Verifying Intent",
+    },
+
+    "Verifying Intent": {
+      entry: "dequeueValidQuote",
+      always: [
+        {
+          target: "Broadcasting Intent",
+          guard: "isIntentRelevant",
+        },
+        {
+          target: "Completed",
+          description: "QUOTE_EXPIRED_RETURN_IS_LOWER",
+
+          actions: [
+            {
+              type: "setError",
+              params: {
+                reason: "ERR_QUOTE_EXPIRED_RETURN_IS_LOWER",
+                error: null,
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    "Broadcasting Intent": {
+      invoke: {
+        src: "broadcastMessage",
+
+        input: ({ context }) => {
+          assert(context.signature != null, "Signature is not set")
+          assert(context.messageToSign != null, "Sign message is not set")
+
+          let quoteHashes: string[] = []
+          if (context.quoteToPublish) {
+            quoteHashes = quoteHashes.concat(context.quoteToPublish.quoteHashes)
+          }
+
+          if (
+            context.intentOperationParams.type === "withdraw" &&
+            context.intentOperationParams.nep141Storage &&
+            context.intentOperationParams.nep141Storage.quote
+          ) {
+            quoteHashes = quoteHashes.concat(
+              context.intentOperationParams.nep141Storage.quote.quoteHashes
+            )
+          }
+
+          return {
+            signatureData: context.signature,
+            userInfo: {
+              userAddress: context.userAddress,
+              userChainType: context.userChainType,
+            },
+            quoteHashes,
+          }
+        },
+
+        onError: {
+          target: "Generic Error",
+          description: "CANNOT_PUBLISH_INTENT",
+
+          actions: [
+            {
+              type: "logError",
+              params: ({ event }) => event,
+            },
+            {
+              type: "setError",
+              params: ({ event }) => ({
+                reason: "ERR_CANNOT_PUBLISH_INTENT",
+                error: toError(event.error),
+              }),
+            },
+          ],
+        },
+
+        onDone: [
+          {
+            target: "Completed",
+            guard: {
+              type: "isOk",
+              params: ({ event }) => event.output,
+            },
+            actions: {
+              type: "setIntentHash",
+              params: ({ event }) => {
+                assert(event.output.tag === "ok")
+                return event.output.value
+              },
+            },
+          },
+          {
+            target: "Generic Error",
+            actions: {
+              type: "setError",
+              params: ({ event }) => {
+                assert(event.output.tag === "err")
+                return {
+                  reason: "ERR_CANNOT_PUBLISH_INTENT",
+                  server_reason: event.output.value.reason,
+                }
+              },
+            },
+          },
+        ],
+      },
+    },
+
+    Completed: {
+      type: "final",
+    },
+
+    "Generic Error": {
+      type: "final",
+    },
+  },
+})
+
+function toError(error: unknown): Error {
+  return error instanceof Error ? error : new Error("unknown error")
+}
