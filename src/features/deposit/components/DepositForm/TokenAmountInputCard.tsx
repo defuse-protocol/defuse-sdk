b5fe5c82af79638a38131c01dfb34da75672429d
jobotics <jo_botics@proton.me>
2025-02-24 15:01:24 +0000

    chore: update otc maker form (#295)
    * chore: add label to TokenAmountInputCard
    
    * chore: add variant to TokenAmountInputCard component

--- src/features/deposit/components/DepositForm/TokenAmountInputCard.tsx
+++ src/features/deposit/components/DepositForm/TokenAmountInputCard.tsx
@@ -1,26 +1,47 @@
 import { type InputHTMLAttributes, type ReactNode, forwardRef } from "react"
 import { AssetComboIcon } from "../../../../components/Asset/AssetComboIcon"
 import type { BaseTokenInfo, UnifiedTokenInfo } from "../../../../types/base"
+import { cn } from "../../../../utils/cn"
 import { isBaseToken } from "../../../../utils/token"
-
 export function TokenAmountInputCard({
+  variant,
   tokenSlot,
   inputSlot,
   balanceSlot,
   priceSlot,
+  labelSlot,
 }: {
+  variant?: "in" | "out"
   tokenSlot?: ReactNode
   inputSlot?: ReactNode
   balanceSlot?: ReactNode
   priceSlot?: ReactNode
+  labelSlot?: ReactNode
 }) {
   return (
-    <div className="flex flex-col gap-2.5 rounded-lg border border-border bg-gray-2 p-4">
+    <div
+      className={cn(
+        "flex flex-col gap-2.5 rounded-lg border border-border bg-gray-2 p-4",
+        variant === "in" && "rounded-[10px] border-0 bg-gray-2",
+        variant === "out" && "rounded-[10px] border-0 bg-gray-3"
+      )}
+    >
+      <div className="flex items-center justify-between gap-4">
+        {/* Label */}
+        <div>{labelSlot}</div>
+      </div>
+
       <div className="flex items-center gap-4">
         {/* Amount Input */}
         <div className="relative flex-1">
           <div className="overflow-hidden">{inputSlot}</div>
-          <div className="pointer-events-none absolute top-0 right-0 bottom-0 w-12 bg-gradient-to-r from-transparent to-gray-2" />
+          <div
+            className={cn(
+              "pointer-events-none absolute top-0 right-0 bottom-0 w-12 bg-gradient-to-r from-transparent to-gray-2",
+              variant === "in" && "to-gray-2",
+              variant === "out" && "to-gray-3"
+            )}
+          />
         </div>
 
         {/* Token Selector */}
@@ -31,7 +52,13 @@ export function TokenAmountInputCard({
         {/* Price */}
         <div className="relative flex-1 overflow-hidden whitespace-nowrap">
           <div>{priceSlot}</div>
-          <div className="pointer-events-none absolute top-0 right-0 bottom-0 w-12 bg-gradient-to-r from-transparent to-gray-2" />
+          <div
+            className={cn(
+              "pointer-events-none absolute top-0 right-0 bottom-0 w-12 bg-gradient-to-r from-transparent to-gray-2",
+              variant === "in" && "to-gray-2",
+              variant === "out" && "to-gray-3"
+            )}
+          />
         </div>
 
         {/* Balance */}
--- src/features/otcDesk/components/OtcMakerForm.tsx
+++ src/features/otcDesk/components/OtcMakerForm.tsx
@@ -227,13 +227,16 @@ export function OtcMakerForm({
       >
         <div className="flex flex-col items-center">
           <div className="flex flex-col gap-3">
-            <label
-              htmlFor="otc-maker-amount-in"
-              className="font-bold text-label text-sm"
-            >
-              Sell
-            </label>
             <TokenAmountInputCard
+              variant="in"
+              labelSlot={
+                <label
+                  htmlFor="otc-maker-amount-in"
+                  className="font-bold text-label text-sm"
+                >
+                  Sell
+                </label>
+              }
               inputSlot={
                 <TokenAmountInputCard.Input
                   id="otc-maker-amount-in"
@@ -290,19 +293,22 @@ export function OtcMakerForm({
               e.preventDefault()
               formValuesRef.trigger.switchTokens()
             }}
-            className="size-10 -my-3.5 rounded-lg bg-gray-50 flex items-center justify-center"
+            className="size-10 -my-3.5 rounded-[10px] bg-accent-1 flex items-center justify-center z-10"
           >
-            <ArrowsDownUp className="size-5" />
+            <ArrowsDownUp className="size-5" weight="bold" />
           </button>
 
           <div className="flex flex-col gap-3">
-            <label
-              htmlFor="otc-maker-amount-out"
-              className="font-bold text-label text-sm"
-            >
-              Buy
-            </label>
             <TokenAmountInputCard
+              variant="out"
+              labelSlot={
+                <label
+                  htmlFor="otc-maker-amount-out"
+                  className="font-bold text-label text-sm"
+                >
+                  Buy
+                </label>
+              }
               inputSlot={
                 <TokenAmountInputCard.Input
                   id="otc-maker-amount-out"
