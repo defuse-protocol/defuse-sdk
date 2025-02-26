a1d13d19c184c296da95239e45b4b77a861ec98c
Old Emil <cawabunga@defuse.org>
2025-02-26 11:34:16 +0100

    refactor: remove unnecessary dialog width manipulation

--- src/components/Modal/ModalDialog/index.tsx
+++ src/components/Modal/ModalDialog/index.tsx
@@ -8,7 +8,6 @@ import {
   useRef,
   useState,
 } from "react"
-import { useResize } from "../../../hooks/useResize"
 import { useModalStore } from "../../../providers/ModalStoreProvider"
 import { WidgetContext } from "../../WidgetRoot"
 
@@ -22,11 +21,7 @@ export const ModalDialog = ({
 }>) => {
   const { onCloseModal } = useModalStore((state) => state)
   const [open, setOpen] = useState(true)
-  const [containerWidth, setContainerWidth] = useState<number>(0)
   const divRef = useRef<HTMLDivElement>(null)
-  const { width } = useResize(divRef)
-
-  const defaultMaxWidth = "512px"
 
   const handleCloseModal = useCallback(() => {
     if (!open) {
@@ -39,16 +34,6 @@ export const ModalDialog = ({
     handleCloseModal()
   }, [handleCloseModal])
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: `divRef.current` will be fixed soon
-  useEffect(() => {
-    if (divRef.current) {
-      setContainerWidth(divRef.current.offsetWidth || 0)
-    }
-    return () => {
-      setContainerWidth(0)
-    }
-  }, [divRef.current, width])
-
   const { portalContainer } = useContext(WidgetContext)
 
   return (
@@ -58,13 +43,6 @@ export const ModalDialog = ({
         className={`fixed bg-white dark:bg-black-800 shadow-lg px-5 pt-5 pb-[max(env(safe-area-inset-bottom,0px),theme(spacing.5))] focus:outline-none
           md:w-[90vw] md:max-w-[472px] md:max-h-[85vh] md:p-5 md:top-1/2 md:bottom-auto md:left-1/2 md:right-auto md:-translate-x-1/2 md:-translate-y-1/2 md:rounded-2xl md:animate-content-show
           bottom-0 left-0 right-0 top-auto max-w-full max-h-[70vh] rounded-t-2xl rounded-b-none animate-slide-up`}
-        maxWidth={
-          containerWidth
-            ? containerWidth < 768
-              ? "100%"
-              : defaultMaxWidth
-            : defaultMaxWidth
-        }
         onOpenAutoFocus={(e) => {
           // This is a workaround for focusing the first input in the modal
           // Focusing first input is annoying for mobile users
