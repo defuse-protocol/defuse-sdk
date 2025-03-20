a2b4edd0994d721a386c49d8ebf8ad165b67049f
Old Emil <cawabunga@defuse.org>
2025-03-20 12:53:32 +0100

    fix: remove queries default settings

--- src/providers/QueryClientProvider.tsx
+++ src/providers/QueryClientProvider.tsx
@@ -3,14 +3,9 @@ import {
   QueryClientProvider as RCProvider,
 } from "@tanstack/react-query"
 import type { PropsWithChildren } from "react"
-import { settings } from "../constants/settings"
 
 // todo: accept queryClient instance from user
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: settings.queries,
-  },
-})
+export const queryClient = new QueryClient()
 
 export const QueryClientProvider: React.FC<PropsWithChildren> = ({
   children,
