92bed29ce115d5181353c896694a89ed8a706cd1
Old Emil <cawabunga@defuse.org>
2025-03-21 14:34:25 +0100

    feat: add AccountWidget component

---
+++ src/features/account/components/AccountWidget.tsx
@@ -0,0 +1,51 @@
+import { WidgetRoot } from "../../../components/WidgetRoot"
+import type { BaseTokenInfo, UnifiedTokenInfo } from "../../../types/base"
+import type { ChainType } from "../../../types/deposit"
+import { userAddressToDefuseUserId } from "../../../utils/defuse"
+import { useWatchHoldings } from "../hooks/useWatchHoldings"
+import { computeTotalUsdValue } from "../utils/holdingsUtils"
+import { HoldingsIsland } from "./HoldingsIsland"
+import { SummaryIsland } from "./SummaryIsland"
+
+export interface AccountWidgetProps {
+  tokenList: (BaseTokenInfo | UnifiedTokenInfo)[]
+
+  userAddress: string | null | undefined
+  userChainType: ChainType | null | undefined
+
+  depositHref: string
+  withdrawHref: string
+  giftHref: string
+}
+
+export function AccountWidget({
+  tokenList,
+  userAddress,
+  userChainType,
+  depositHref,
+  withdrawHref,
+  giftHref,
+}: AccountWidgetProps) {
+  const userId =
+    userAddress != null && userChainType != null
+      ? userAddressToDefuseUserId(userAddress, userChainType)
+      : null
+
+  const holdings = useWatchHoldings({ userId, tokenList })
+  const totalValueUsd = holdings ? computeTotalUsdValue(holdings) : undefined
+
+  return (
+    <WidgetRoot>
+      <div className="widget-container flex flex-col gap-5">
+        <SummaryIsland
+          valueUsd={totalValueUsd}
+          depositHref={depositHref}
+          withdrawHref={withdrawHref}
+          giftHref={giftHref}
+        />
+
+        <HoldingsIsland holdings={holdings} />
+      </div>
+    </WidgetRoot>
+  )
+}
