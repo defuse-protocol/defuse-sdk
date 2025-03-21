114e5552706846f172b1ee471b6e3cf745d35e50
jobotics <jo_botics@proton.me>
2025-03-21 11:33:49 +0000

    chore: remove MessageHint

--- src/features/gift/components/GiftMakerForm.tsx
+++ src/features/gift/components/GiftMakerForm.tsx
@@ -26,7 +26,6 @@ import { GiftMakerReadyDialog } from "./GiftMakerReadyDialog"
 import { GiftMessageInput } from "./GiftMessageInput"
 import { GiftDescription } from "./shared/GiftDescription"
 import { GiftHeader } from "./shared/GiftHeader"
-import { MessageHint } from "./shared/MessageHint"
 
 export type GiftMakerWidgetProps = {
   /** List of available tokens for trading */
@@ -249,12 +248,6 @@ export function GiftMakerForm({
                 />
               }
             />
-            {formValues.message && (
-              <MessageHint
-                text="Message is publicly readable and permanently stored on the blockchain. Anyone can view this message."
-                className="mt-4"
-              />
-            )}
           </div>
         </div>
 
