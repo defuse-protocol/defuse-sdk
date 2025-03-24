13bdc3338b2fc0d94343b41c2c4ad7e76fac9e49
jobotics <jo_botics@proton.me>
2025-03-24 00:13:20 +0000

    chore: update GiftClaimedMessage

--- src/features/gift/components/shared/GiftClaimedMessage.tsx
+++ src/features/gift/components/shared/GiftClaimedMessage.tsx
@@ -1,15 +1,10 @@
-import { Callout } from "@radix-ui/themes"
-
 export function GiftClaimedMessage() {
   return (
-    <Callout.Root className="bg-warning px-3 py-2 text-warning-foreground mt-5">
-      <Callout.Text className="text-xs">
-        <span className="font-bold">
-          Your gift is being claimed.
-          {/* biome-ignore lint/nursery/useConsistentCurlyBraces: <explanation> */}
-        </span>{" "}
-        <span>This may take 5-10 seconds more.</span>
-      </Callout.Text>
-    </Callout.Root>
+    <div className="bg-warning px-3 py-2 text-warning-foreground mt-2.5">
+      <div className="text-xs flex items-center justify-center">
+        <span className="font-bold">Your gift is being claimed.</span>
+        <span className="ml-2">This may take 5-10 seconds more.</span>
+      </div>
+    </div>
   )
 }
