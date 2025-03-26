6c3639e655f0c86102575e174d663585eead45fc
jobotics <jo_botics@proton.me>
2025-03-26 01:59:55 +0000

    fix: remove default Chrome focus-visible blue outline on inputs

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,7 +18,7 @@ export const SearchBar = ({
         <MagnifyingGlassIcon width={22} height={22} />
       </div>
       <input
-        className="flex-1 border-transparent p-0 focus:border-transparent focus:ring-0 bg-gray-3 dark:text-white"
+        className="flex-1 border-transparent p-0 focus:border-transparent focus:ring-0 bg-gray-3 dark:text-white outline-none"
         placeholder={placeholder}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
