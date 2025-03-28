b6ea7bf54c2585ddea2355f5ff9743fb7f89bada
jobotics <jo_botics@proton.me>
2025-03-28 04:28:18 +0000

    test: check schema v2

--- src/features/gift/utils/schemaStorage.test.ts
+++ src/features/gift/utils/schemaStorage.test.ts
@@ -1,6 +1,6 @@
 import * as v from "valibot"
 import { describe, expect, it } from "vitest"
-import { GiftStorageSchema } from "./schemaStorage"
+import { GiftStorageSchema, GiftStorageSchemaV2 } from "./schemaStorage"
 
 describe("GiftStorageSchema", () => {
   it("valid gift storage data", () => {
@@ -23,6 +23,9 @@ describe("GiftStorageSchema", () => {
                 icon: "https://example.com/usdc.png",
                 chainIcon: "https://example.com/usdc.png",
                 chainName: "near",
+                chainId: "1",
+                routes: ["route1", "route2"],
+                bridge: "bridge",
               },
               secretKey: "ed25519:secretKey",
               accountId: "accountId",
@@ -34,6 +37,32 @@ describe("GiftStorageSchema", () => {
       },
     }
     const result = v.parse(GiftStorageSchema, giftStorageData)
+    expect(result).toEqual(giftStorageData)
+  })
+
+  it("valid gift storage data v2", () => {
+    const giftStorageData = {
+      state: {
+        gifts: {
+          user1: [
+            {
+              giftId: "1",
+              giftStatus: "preparing",
+              intentHashes: ["hash1"],
+              tokenDiff: {
+                "nep141:usdc": 1000n,
+              },
+              tokenId: "usdc",
+              secretKey: "ed25519:secretKey",
+              accountId: "accountId",
+              message: "message",
+              updatedAt: 1742910077547,
+            },
+          ],
+        },
+      },
+    }
+    const result = v.parse(GiftStorageSchemaV2, giftStorageData)
     expect(result).toEqual(giftStorageData)
   })
 })
