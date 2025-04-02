4e9b7389024e223fdf6bff26f1a8898d9ca3c2c4
Old Emil <cawabunga@defuse.org>
2025-04-02 11:52:10 +0200

    refactor: rename NEP141_STORAGE_TOKEN to NEP141_STORAGE_TOKEN_ID (#372)

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,12 +1,4 @@
 import type { BaseTokenInfo } from "../types/base"
 
-export const NEP141_STORAGE_TOKEN: BaseTokenInfo = {
-  defuseAssetId: "nep141:wrap.near",
-  address: "wrap.near",
-  decimals: 24,
-  icon: "https://assets.coingecko.com/coins/images/10365/standard/near.jpg",
-  chainName: "near",
-  bridge: "direct",
-  symbol: "NEAR",
-  name: "Near",
-}
+export const NEP141_STORAGE_TOKEN_ID: BaseTokenInfo["defuseAssetId"] =
+  "nep141:wrap.near"
