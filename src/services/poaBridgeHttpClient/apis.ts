4d8bb8e7db765d217080ed8e3547221fe20e5cf1
Old Emil <cawabunga@defuse.org>
2025-04-08 18:04:38 +0200

    refactor: improve parsing RPC response and throwing RPC error

--- src/services/poaBridgeHttpClient/apis.ts
+++ src/services/poaBridgeHttpClient/apis.ts
@@ -5,52 +5,50 @@ export async function getSupportedTokens(
   params: types.GetSupportedTokensRequest["params"][0],
   config: types.RequestConfig = {}
 ): Promise<types.GetSupportedTokensResponse["result"]> {
-  const json = await jsonRPCRequest<types.GetSupportedTokensRequest>(
+  const result = await jsonRPCRequest<types.GetSupportedTokensRequest>(
     "supported_tokens",
     params,
     config
   )
-  return json.result
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  return result as any
 }
 
 export async function getDepositAddress(
   params: types.GetDepositAddressRequest["params"][0],
   config: types.RequestConfig = {}
 ): Promise<types.GetDepositAddressResponse["result"]> {
-  const json = await jsonRPCRequest<types.GetDepositAddressRequest>(
+  const result = await jsonRPCRequest<types.GetDepositAddressRequest>(
     "deposit_address",
     params,
     config
   )
-  return json.result
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  return result as any
 }
 
 export async function getDepositStatus(
   params: types.GetDepositStatusRequest["params"][0],
   config: types.RequestConfig = {}
 ): Promise<types.GetDepositStatusResponse["result"]> {
-  const json = await jsonRPCRequest<types.GetDepositStatusRequest>(
+  const result = await jsonRPCRequest<types.GetDepositStatusRequest>(
     "recent_deposits",
     params,
     config
   )
-  return json.result ?? { deposits: [] }
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  return (result as any) ?? { deposits: [] }
 }
 
 export async function getWithdrawalStatus(
   params: types.WithdrawalStatusRequest["params"][0],
   config: types.RequestConfig = {}
 ): Promise<types.WithdrawalStatusResponseOk["result"]> {
-  const json:
-    | types.WithdrawalStatusResponseOk
-    | types.WithdrawalStatusResponseErr =
-    await jsonRPCRequest<types.WithdrawalStatusRequest>(
-      "withdrawal_status",
-      params,
-      config
-    )
-  if ("error" in json) {
-    throw new Error(json.error)
-  }
-  return json.result
+  const result = await jsonRPCRequest<types.WithdrawalStatusRequest>(
+    "withdrawal_status",
+    params,
+    config
+  )
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  return result as any
 }
