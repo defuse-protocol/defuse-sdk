ca2b9df65818b439d522a9975b69b7bdde4489cc
jobotics <jo_botics@proton.me>
2025-04-08 15:26:52 +0100

    chore: update min amount hint placement

--- src/features/deposit/components/DepositForm/renderDepositHint.tsx
+++ src/features/deposit/components/DepositForm/renderDepositHint.tsx
@@ -21,7 +21,6 @@ const networkSelectToLabel: Record<BlockchainEnum, string> = {
 
 export function renderDepositHint(
   network: BlockchainEnum,
-  minDepositAmount: bigint | null,
   token: BaseTokenInfo
 ) {
   return (
@@ -40,19 +39,22 @@ export function renderDepositHint(
           </span>
         </Callout.Text>
       </Callout.Root>
+    </div>
+  )
+}
 
-      {minDepositAmount != null && (
-        <div className="flex flex-col gap-3.5 font-medium text-gray-11 text-xs">
-          <div className="flex justify-between">
-            <div>Minimum deposit</div>
-            <div className="text-label">
-              {/* biome-ignore lint/nursery/useConsistentCurlyBraces: space is needed here */}
-              {formatTokenValue(minDepositAmount, token.decimals)}{" "}
-              {token.symbol}
-            </div>
-          </div>
+export function renderMinDepositAmountHint(
+  minDepositAmount: bigint,
+  token: BaseTokenInfo
+) {
+  return (
+    <div className="flex flex-col gap-3.5 font-medium text-gray-11 text-xs">
+      <div className="flex justify-between">
+        <div>Minimum deposit</div>
+        <div className="text-label">
+          {formatTokenValue(minDepositAmount, token.decimals)} {token.symbol}
         </div>
-      )}
+      </div>
     </div>
   )
 }
