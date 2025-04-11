ddbce0929950da895bb51bd1b20e480f5f44f16f
dr-block <artur.movsisyan@defuse.org>
2025-04-11 19:00:15 +0400

    Withdraw display bridge hot wallet liquidity fix (#422)
    * fix: withdraw - display bridge's hot wallet's liquidity, fixes for incorrect return type
    
    * fix: withdraw - display bridge's hot wallet's liquidity, optimize logic
    
    ---------
    
    Co-authored-by: dr-block <dr-block@defuse.org>

--- src/features/withdraw/components/WithdrawForm/utils.ts
+++ src/features/withdraw/components/WithdrawForm/utils.ts
@@ -61,10 +61,13 @@ export const getAvailableBlockchains = (token: SwappableToken) =>
 export const shouldShowHotBalance = (
   balances: { [address: string]: TokenBalances },
   tokenInBalance?: TokenValue
-) => {
+): boolean => {
   const { amount: userBalance, decimals: userBalanceDecimals } =
     tokenInBalance ?? { amount: 0n, decimals: 1 }
-  let showHotBalances = userBalance > 0
+  const userHasAnyBalance = userBalance > 0
+  if (!userHasAnyBalance) {
+    return false
+  }
 
   let anyHotBalanceIsLessThanUserBalance = false
   for (const address in balances) {
@@ -81,11 +84,7 @@ export const shouldShowHotBalance = (
     }
   }
 
-  showHotBalances = showHotBalances && anyHotBalanceIsLessThanUserBalance
-
-  return {
-    showHotBalances,
-  }
+  return anyHotBalanceIsLessThanUserBalance
 }
 
 export const getBlockchainSelectItems = (
