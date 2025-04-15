165a640ebd62e410f3e7fcb9e7647221a973cc2d
Old Emil <cawabunga@defuse.org>
2025-04-15 11:22:06 +0200

    refactor: update imports

--- src/features/account/queries/balanceQueries.ts
+++ src/features/account/queries/balanceQueries.ts
@@ -1,7 +1,7 @@
 import type { QueryObserverOptions } from "@tanstack/react-query"
 import { nearClient } from "../../../constants/nearClient"
+import { getPendingDeposits } from "../../../sdk/poaBridge/poaBridgeService"
 import { getDepositedBalances } from "../../../services/defuseBalanceService"
-import { getPendingDeposits } from "../../../services/poaBridgeService"
 import type { IntentsUserId } from "../../../types/intentsUserId"
 import { assert } from "../../../utils/assert"
 import type { BalanceMapping } from "../../machines/depositedBalanceMachine"
--- src/features/machines/depositedBalanceMachine.ts
+++ src/features/machines/depositedBalanceMachine.ts
@@ -10,8 +10,8 @@ import {
   setup,
 } from "xstate"
 import { queryClient } from "../../providers/QueryClientProvider"
+import { getPendingDeposits } from "../../sdk/poaBridge/poaBridgeService"
 import { getDepositedBalances } from "../../services/defuseBalanceService"
-import { getPendingDeposits } from "../../services/poaBridgeService"
 import type { AuthMethod } from "../../types/authHandle"
 import type {
   BaseTokenInfo,
