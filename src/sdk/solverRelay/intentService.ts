a43a774cfa229cf25c15c477e66b6f2a6ef0c01d
Old Emil <cawabunga@defuse.org>
2025-04-15 12:56:23 +0200

    refactor: move intentService.ts to relaySolver folder

--- src/sdk/solverRelay/intentService.ts
+++ src/sdk/solverRelay/intentService.ts
@@ -1,14 +1,14 @@
 import { retry } from "@lifeomic/attempt"
 import { Err, Ok, type Result } from "@thames/monads"
-import { BaseError } from "../errors/base"
-import { HttpRequestError } from "../errors/request"
-import { logger } from "../logger"
-import * as solverRelayClient from "../sdk/solverRelayHttpClient"
-import type * as types from "../sdk/solverRelayHttpClient/types"
-import type { AuthMethod } from "../types/authHandle"
-import type { WalletSignatureResult } from "../types/walletMessage"
-import { prepareSwapSignedData } from "../utils/prepareBroadcastRequest"
-import { wait } from "../utils/wait"
+import { BaseError } from "../../errors/base"
+import { HttpRequestError } from "../../errors/request"
+import { logger } from "../../logger"
+import type { AuthMethod } from "../../types/authHandle"
+import type { WalletSignatureResult } from "../../types/walletMessage"
+import { prepareSwapSignedData } from "../../utils/prepareBroadcastRequest"
+import { wait } from "../../utils/wait"
+import * as solverRelayClient from "../solverRelayHttpClient"
+import type * as types from "../solverRelayHttpClient/types"
 
 export type PublishIntentResult =
   | { tag: "ok"; value: string }
