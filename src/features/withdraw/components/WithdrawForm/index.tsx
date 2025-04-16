a40a047836d313ba6da49c35637ff91f32a3e09c
Hyp3r <hypersane1984@proton.me>
2025-04-16 16:30:07 +0100

    Feat/hot balances withdrawal modal (#432)
    * fix: port the hot balance display to the new network modal
    
    * fix: remove wrong HotBalance display outside of the list

--- src/features/withdraw/components/WithdrawForm/index.tsx
+++ src/features/withdraw/components/WithdrawForm/index.tsx
@@ -24,8 +24,10 @@ import type { intentStatusMachine } from "src/features/machines/intentStatusMach
 import { useModalController } from "src/hooks/useModalController"
 import { useTokensUsdPrices } from "src/hooks/useTokensUsdPrices"
 import { useTokensStore } from "src/providers/TokensStoreProvider"
+import type { BlockchainEnum } from "src/sdk/poaBridge/constants/blockchains"
 import type { PreparationOutput } from "src/services/withdrawService"
 import { ModalType } from "src/stores/modalStore"
+import { reverseAssetNetworkAdapter } from "src/utils/adapters"
 import { formatTokenValue, formatUsdAmount } from "src/utils/format"
 import getTokenUsdPrice from "src/utils/getTokenUsdPrice"
 import { getTokenMaxDecimals } from "src/utils/tokenUtils"
@@ -461,13 +463,6 @@ export const WithdrawForm = ({
                         Object.values(blockchainSelectItems)[0]?.value
                     }
                   />
-                  {showHotBalances && (
-                    <HotBalance
-                      hotBalance={
-                        blockchainSelectItems[field.value]?.hotBalance
-                      }
-                    />
-                  )}
 
                   <ModalSelectNetwork
                     token={token}
@@ -475,6 +470,21 @@ export const WithdrawForm = ({
                     selectedNetwork={blockchain}
                     isOpen={isNetworkModalOpen}
                     onClose={onCloseNetworkModal}
+                    renderValueDetails={
+                      showHotBalances
+                        ? (address: string) => (
+                            <HotBalance
+                              hotBalance={
+                                blockchainSelectItems[
+                                  reverseAssetNetworkAdapter[
+                                    address as BlockchainEnum
+                                  ]
+                                ]?.hotBalance
+                              }
+                            />
+                          )
+                        : undefined
+                    }
                   />
                 </>
               )}
