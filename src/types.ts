dfaa80c6732752644986ee616dcbeb8d98f811bb
dr-block <dr-block@defuse.org>
2025-05-15 13:56:34 +0400

    feat: check solver liquidity - add new export types task: 86etfe2qf

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,4 @@
 export type { BaseTokenInfo, UnifiedTokenInfo } from "./types/base"
+export type { AggregatedQuote } from "./services/quoteService"
+export type { AggregatedQuoteErr } from "./features/otcDesk/utils/quoteUtils"
 export { AuthMethod } from "./types/authHandle"
