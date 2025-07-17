2235226d7850a5dbc55952ac7fc460621bf0da6a
jobotics <jo_botics@proton.me>
2025-07-17 22:56:35 +0100

    refactor: migrate toError

--- src/features/gift/actors/giftMakerRootMachine.ts
+++ src/features/gift/actors/giftMakerRootMachine.ts
@@ -1,3 +1,4 @@
+import { errors } from "@defuse-protocol/internal-utils"
 import {
   type ActorRefFrom,
   type DoneActorEvent,
@@ -19,7 +20,6 @@ import type {
   WalletSignatureResult,
 } from "../../../types/walletMessage"
 import { assert } from "../../../utils/assert"
-import { toError } from "../../../utils/errors"
 import {
   type Events as DepositedBalanceEvents,
   depositedBalanceMachine,
@@ -218,7 +218,7 @@ export const giftMakerRootMachine = setup({
   },
   actions: {
     logError: (_, event: { error: unknown }) => {
-      const err = toError(event.error)
+      const err = errors.toError(event.error)
       logger.error(err)
     },
     setError: assign({
