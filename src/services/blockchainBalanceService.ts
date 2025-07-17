2be132ae095cbf0e49713ac6f777975c9f20517e
jobotics <jo_botics@proton.me>
2025-07-17 21:12:40 +0100

    refactor: migrate getNearNep141[Min]StorageBalance utils

--- src/services/blockchainBalanceService.ts
+++ src/services/blockchainBalanceService.ts
@@ -1,4 +1,3 @@
-import { base64 } from "@scure/base"
 import { AccountLayout } from "@solana/spl-token"
 import { Connection, PublicKey } from "@solana/web3.js"
 import { Address as TonAddress, beginCell } from "@ton/ton"
@@ -65,57 +64,6 @@ export const getNearNep141Balance = async ({
     )
     return null
   }
-}
-
-export const getNearNep141StorageBalance = async ({
-  contractId,
-  accountId,
-}: {
-  contractId: string
-  accountId: string
-}): Promise<bigint> => {
-  try {
-    const args = { account_id: accountId }
-    const argsBase64 = Buffer.from(JSON.stringify(args)).toString("base64")
-
-    const response = await nearClient.query({
-      request_type: "call_function",
-      method_name: "storage_balance_of",
-      account_id: contractId,
-      args_base64: argsBase64,
-      finality: "optimistic",
-    })
-
-    const parsed = decodeQueryResult(
-      response,
-      v.union([v.null(), v.object({ total: v.string() })])
-    )
-
-    return BigInt(parsed?.total || "0")
-  } catch (err: unknown) {
-    throw new Error("Error fetching balance", { cause: err })
-  }
-}
-
-export const getNearNep141MinStorageBalance = async ({
-  contractId,
-}: {
-  contractId: string
-}): Promise<bigint> => {
-  const response = await nearClient.query({
-    request_type: "call_function",
-    method_name: "storage_balance_bounds",
-    account_id: contractId,
-    args_base64: base64.encode(new TextEncoder().encode(JSON.stringify({}))),
-    finality: "optimistic",
-  })
-
-  const parsed = decodeQueryResult(
-    response,
-    v.object({ min: v.string(), max: v.string() })
-  )
-
-  return BigInt(parsed.min)
 }
 
 export const getEvmNativeBalance = async ({
